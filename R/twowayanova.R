0963f16845ebea4bd261719b53cb2606a5cf90e6
TimBock <tim.bock@numbers.net.au>
2016-01-15 18:02:00 +1100

    Additional examples of two way ANOVA

--- R/twowayanova.R
+++ R/twowayanova.R
@@ -1,15 +1,23 @@
 
-
-### Note that the order determines significance.
+# Comments for Documentation
+#1. Note that order of variables in the model determines significance.
+#2. SPSS does not produce two-way post hoc comparisons.  That is, the outputs in SPSS test differences between the observed mean scores, and not
+# between the actual effects. But, their standard errors look right, so perhaps can just substitute and do as an option.
+# Robust stanard errors are possible: glht(vcov = sandwich)
+#Note that the order determines significance.
 
 # data(colas)
 # names(colas)
 #
+# library(multcomp)
 # lmod <- aov(unclass(q1a) ~ d1 + d2, data = colas)
 # lhypotheses <- glht(lmod,  linfct = mcp(d2 = "Tukey")) # Tukey refers to the contrasts, not the correction.
 # summary(lhypotheses)
 # summary(lhypotheses, test = adjusted("bonferroni"))
-# summary(lhypotheses, test = adjusted("fdr"))
+# summary(lhypotheses, test = univariate()) #Uncorrected (i.e., Fished LSD)
+
+
+
 #
 #
 #
