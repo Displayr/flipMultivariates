aa24b2485906bff276286d39f02a6944e8d094fb
TimBock <tim.bock@numbers.net.au>
2016-04-21 16:50:19 +1000

    tests for transformations

--- tests/testthat/test-transformations.R
+++ tests/testthat/test-transformations.R
@@ -4,4 +4,6 @@ data(bank)
 test_that("Dichotomizing works",
 {
     expect_true(all(table(DichotomizeFactor(factor(LETTERS[1:10]))) == c(5,5)))
+    expect_true(all(table(DichotomizeFactor(bank$Overall)) == c(265, 497)))
+
 })
