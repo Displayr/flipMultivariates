bb275ce967fbe6b90abdf169495d847846f32bd7
Chris Facer <chris.facer@q-researchsoftware.com>
2016-05-05 14:17:47 +1000

    Updates to pretty regression printing
    Simplified code which rounds numbers in the table.
    Changed default number of decimal places to use.

--- R/regression.R
+++ R/regression.R
@@ -644,7 +644,10 @@ Observed.Regression <- function(x)
 
 # Create an HTML widget data table (package DT) from the coefficients
 # table in a regression summary.
-createRegressionDataTable <- function(x, p.cutoff, caption = NULL)
+createRegressionDataTable <- function(x, p.cutoff, caption = NULL, coeff.digits = 2,
+                                      p.digits = 2, coefficient.indices = 1:2,
+                                      test.index = 3, p.index = 4,
+                                      eps.p = 0.001)
 {
     # Given a table of coefficients from a summary of a regression
     # figure out which test has been used and which column the test
@@ -673,109 +676,42 @@ createRegressionDataTable <- function(x, p.cutoff, caption = NULL)
 
     # Create a formatted array of regression coefficient information that can be passed to an HTMLwidget
     # DataTable.
-    # This function is a copy of \code{\link{printCoefmat}} which returns the formatted output
-    # instead of printing it, and which does not do the significance testing stars by default.
-    .formatRegressionCoefficientMatrix <- function (x, digits = max(3L, getOption("digits") - 2L), signif.stars = FALSE,
-                                                    signif.legend = signif.stars, dig.tst = max(1L, min(5L, digits -
-                                                                                                            1L)), cs.ind = 1:k, tst.ind = k + 1, zap.ind = integer(),
-                                                    P.values = NULL, has.Pvalue = nc >= 4 && substr(colnames(x)[nc],
-                                                                                                    1, 3) == "Pr(", eps.Pvalue = .Machine$double.eps, na.print = "NA",
-                                                    ...)
+    .formatRegressionCoefficientMatrix <- function (x, coeff.digits = 2,
+                                                    p.digits = 2, coefficient.indices = 1:2,
+                                                    test.index = 3, p.index = 4,
+                                                    eps.p = 0.001)
     {
-        if (is.null(d <- dim(x)) || length(d) != 2L)
-            stop("'x' must be coefficient matrix/data frame")
-        nc <- d[2L]
-        if (is.null(P.values)) {
-            scp <- getOption("show.coef.Pvalues")
-            if (!is.logical(scp) || is.na(scp)) {
-                warning("option \"show.coef.Pvalues\" is invalid: assuming TRUE")
-                scp <- TRUE
-            }
-            P.values <- has.Pvalue && scp
-        }
-        else if (P.values && !has.Pvalue)
-            stop("'P.values' is TRUE, but 'has.Pvalue' is not")
-        if (has.Pvalue && !P.values) {
-            d <- dim(xm <- data.matrix(x[, -nc, drop = FALSE]))
-            nc <- nc - 1
-            has.Pvalue <- FALSE
-        }
-        else xm <- data.matrix(x)
-        k <- nc - has.Pvalue - (if (missing(tst.ind))
-            1
-            else length(tst.ind))
-        if (!missing(cs.ind) && length(cs.ind) > k)
-            stop("wrong k / cs.ind")
-        Cf <- array("", dim = d, dimnames = dimnames(xm))
-        ok <- !(ina <- is.na(xm))
-        for (i in zap.ind) xm[, i] <- zapsmall(xm[, i], digits)
-        if (length(cs.ind)) {
-            acs <- abs(coef.se <- xm[, cs.ind, drop = FALSE])
-            if (any(ia <- is.finite(acs))) {
-                digmin <- 1 + if (length(acs <- acs[ia & acs != 0]))
-                    floor(log10(range(acs[acs != 0], finite = TRUE)))
-                else 0
-                Cf[, cs.ind] <- format(round(coef.se, max(1L, digits -
-                                                              digmin)), digits = digits)
-            }
-        }
-        if (length(tst.ind))
-            Cf[, tst.ind] <- format(round(xm[, tst.ind], digits = dig.tst),
-                                    digits = digits)
-        if (any(r.ind <- !((1L:nc) %in% c(cs.ind, tst.ind, if (has.Pvalue) nc))))
-            for (i in which(r.ind)) Cf[, i] <- format(xm[, i], digits = digits)
-        ok[, tst.ind] <- FALSE
-        okP <- if (has.Pvalue)
-            ok[, -nc]
-        else ok
-        x1 <- Cf[okP]
-        dec <- getOption("OutDec")
-        if (dec != ".")
-            x1 <- chartr(dec, ".", x1)
-        x0 <- (xm[okP] == 0) != (as.numeric(x1) == 0)
-        if (length(not.both.0 <- which(x0 & !is.na(x0)))) {
-            Cf[okP][not.both.0] <- format(xm[okP][not.both.0], digits = max(1L,
-                                                                            digits - 1L))
-        }
-        if (any(ina))
-            Cf[ina] <- na.print
-        if (P.values) {
-            if (!is.logical(signif.stars) || is.na(signif.stars)) {
-                warning("option \"show.signif.stars\" is invalid: assuming TRUE")
-                signif.stars <- TRUE
-            }
-            if (any(okP <- ok[, nc])) {
-                pv <- as.vector(xm[, nc])
-                Cf[okP, nc] <- format.pval(pv[okP], digits = dig.tst,
-                                           eps = eps.Pvalue)
-                signif.stars <- signif.stars && any(pv[okP] < 0.1)
-                if (signif.stars) {
-                    Signif <- symnum(pv, corr = FALSE, na = FALSE,
-                                     cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
-                                     symbols = c("***", "**", "*", ".", " "))
-                    Cf <- cbind(Cf, format(Signif))
-                }
-            }
-            else signif.stars <- FALSE
-        }
-        else signif.stars <- FALSE
-        if (signif.stars && signif.legend) {
-            if ((w <- getOption("width")) < nchar(sleg <- attr(Signif,
-                                                               "legend")))
-                sleg <- strwrap(sleg, width = w - 2, prefix = "  ")
-            cat("---\nSignif. codes:  ", sleg, sep = "", fill = w +
-                    4 + max(nchar(sleg, "bytes") - nchar(sleg)))
-        }
-        return(as.data.frame(Cf))
+        d <- dim(x)
+        num.cols <- d[2]
+
+        coefficients <- data.matrix(x)
+
+        tidied.coefficients <- array("", dim = d, dimnames = dimnames(coefficients))
+
+        na.values <- is.na(coefficients)
+
+        normal.indices <- c(coefficient.indices, test.index)
+        tidied.coefficients[,normal.indices] <- format(round(coefficients[, normal.indices], digits = coeff.digits), digits = coeff.digits)
+        tidied.coefficients[,p.index] <- format.pval(coefficients[, p.index], digits = p.digits, eps = eps.p)
+
+        # Print any NA values
+        if (any(na.values))
+            tidied.coefficients[na.values] <- "NA"
+
+        return(as.data.frame(tidied.coefficients))
     }
+
 
 
     # Make a pretty table with a caption
     coefs <- x$summary$coefficients
-    pretty.coefs <- .formatRegressionCoefficientMatrix(coefs)
+    pretty.coefs <- .formatRegressionCoefficientMatrix(coefs, coeff.digits,
+                                                       p.digits, coefficient.indices,
+                                                       test.index, p.index,
+                                                       eps.p)
     pretty.coefs <- as.data.frame(pretty.coefs, stringsAsFactors = FALSE)
 
-    caption <- paste0(caption, "Results highlighted when p < " , p.cutoff)
+    caption <- paste0(caption, "Results highlighted when p <= " , p.cutoff)
 
 
     dt <- flipU::DataTableWithRItemFormat(pretty.coefs,
@@ -784,10 +720,10 @@ createRegressionDataTable <- function(x, p.cutoff, caption = NULL)
                                           page.length = nrow(pretty.coefs),
                                           allow.paging = FALSE,
                                           show.info = FALSE)
-    test.info <- .findTestInCoefficientTable(pretty.coefs)
+
 
     # Highlight significant coefficients
-
+    test.info <- .findTestInCoefficientTable(coefs)
     if (test.info$test.type == "t")
     {
         t.val <- qt(p.cutoff / 2, df = x$original$df.residual)
