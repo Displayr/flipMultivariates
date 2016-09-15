59e64b3c67a6bca1197b40e17a5aeafd112e655e
TimBock <tim.bock@numbers.net.au>
2016-09-15 13:38:17 +1000

    Adding unit tests

--- tests/testthat/test-randomforest.R
+++ tests/testthat/test-randomforest.R
@@ -9,6 +9,27 @@ hair1$split60 <- hair$split60
 hair1$id <- hair$id
     hair1$num <- flipTransformations::AsNumeric(hair1$x1, binary = FALSE)
 
+test_that("Print Random forests",{
+    hair1$numeric <- hair1$num + runif(length(hair1$num)) / 10
+    attr(hair1$x7, "question") <- "Variable number 7"
+    z <- RandomForest(numeric ~ x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18, show.labels = TRUE, output = "Importance", data = hair1, subset = split60 == "Estimation Sample")
+    expect_error(z, NA)
+    z <- RandomForest(numeric ~ x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18, show.labels = FALSE, output = "Importance", data = hair1, subset = split60 == "Estimation Sample")
+    expect_error(z, NA)
+    z <- RandomForest(numeric ~ x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18, show.labels = TRUE, output = "Detail", data = hair1, subset = split60 == "Estimation Sample")
+    expect_error(z, NA)
+    z <- RandomForest(numeric ~ x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18, show.labels = FALSE, output = "Detail", data = hair1, subset = split60 == "Estimation Sample")
+    expect_error(z, NA)
+    hair1$cat <- factor(hair1$num)
+    z <- RandomForest(cat ~ x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18, show.labels = TRUE, output = "Importance", data = hair1, subset = split60 == "Estimation Sample")
+    expect_error(z, NA)
+    z <- RandomForest(cat ~ x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18, show.labels = FALSE, output = "Importance", data = hair1, subset = split60 == "Estimation Sample")
+    expect_error(z, NA)
+    z <- RandomForest(cat ~ x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18, show.labels = TRUE, output = "Detail", data = hair1, subset = split60 == "Estimation Sample")
+    expect_error(z, NA)
+    z <- RandomForest(cat ~ x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18, show.labels = FALSE, output = "Detail", data = hair1, subset = split60 == "Estimation Sample")
+    expect_error(z, NA)
+})
 
 library(flipRegression)
 test_that("Random forests",{
