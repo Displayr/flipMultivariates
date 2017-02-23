43c9e3d5c120be19034103f03791f28e70cce7f4
jakehoare <jake_hoare@hotmail.com>
2017-02-23 11:37:40 +1100

    Added probabilities for SVM

--- R/variables.R
+++ R/variables.R
@@ -83,6 +83,7 @@ randomForestExtractVariables <- function(object, type, newdata = object$model, n
     predict(object$original, type, newdata = newdata, na.action = na.action)
 }
 
+
 #' \code{predict.SupportVectorMachine}
 #'
 #' Predicts values for numeric outcomes and group membership for categories based on new data or the data
@@ -94,61 +95,16 @@ randomForestExtractVariables <- function(object, type, newdata = object$model, n
 #' @param na.action Function determining what should be done with missing values in \code{newdata}.
 #' The default is to predict \code{NA}.
 #' @param ... Additional arguments to pass to predict.SupportVectorMachine.
-#' @importFrom stats na.pass na.omit complete.cases
+#' @importFrom stats na.omit complete.cases
 #' @export
 predict.SupportVectorMachine <- function(object, newdata = NULL, na.action = na.omit, ...)
 {
-    # if (is.null(newdata))
-    # {
-    #     newdata <- object$model
-    #     predicting.training <- TRUE
-    # }
-    # else
-    #     predicting.training <- FALSE
-    #
-    # # EstimationData removes unused levels from training data (after filter and removing NA). svm.predict() binary
-    # # encodes factor variables according to the number of their levels and will fail if newdata contains
-    # # a different number of levels from training data (even if no instances have the unused levels).
-    # # Hence we filter out instances of newdata with new levels (predicting NA), and add back to newdata any
-    # # levels not present but were in training data.  Thus droplevels(newdata) is aligned with training levels.
-    #
-    # training <- object$model[object$subset, names(object$model) != object$outcome.name, drop = FALSE]
-    # train.levels <- sapply(droplevels(training), levels)
-    #
-    # newdata <- newdata[, names(newdata) %in% names(training), drop = FALSE]
-    # #newdata <- newdata[, names(newdata) != object$outcome.name, drop = FALSE]
-    # prediction.levels <- sapply(newdata, levels)
-    # exclusions <- rep(FALSE, nrow(newdata))
-    #
-    # if (!identical(names(training), names(newdata)))
-    #     stop("Attempting to predict based on different variables than those used to train the model.")
-    #
-    # for (i in 1:length(train.levels))
-    # {
-    #     if (!is.null(train.levels[[i]]))    # factor variables only
-    #     {
-    #         # if there are any prediction levels that have not been used to train
-    #         new.levels <- setdiff(prediction.levels[[i]], train.levels[[i]])
-    #         if (!identical(new.levels, character(0)))
-    #         {
-    #             # set exclusions to TRUE for any newdata row with a new factor level
-    #             nb.excluded <- sum(exclusions == TRUE)
-    #             exclusions[newdata[, i] %in% new.levels] <- TRUE
-    #             newly.excluded <- sum(exclusions == TRUE) - nb.excluded
-    #             if (newly.excluded > 0 & !predicting.training)
-    #                 warning(sprintf("Prediction variable %s contains categories (%s) that were not used for training. %d instances are affected and will be predicted NA.",
-    #                                 names(training[i]), new.levels, newly.excluded))
-    #         }
-    #         # if train has any levels not in prediction, then add those levels to prediction
-    #         if (!identical(setdiff(train.levels[[i]], prediction.levels[[i]]), character(0)))
-    #             levels(newdata[, i]) <- train.levels[[i]]
-    #     }
-    # }
-
+    # AlignPredictionVariables is still required without newdata because predictions in object$fitted may be
+    # a subset of object$model.
     newdata <- AlignPredictionVariables(object, newdata)
-
-    # Since e1071 svm predictions cannot return NA for missing data, predict only for complete.cases and
+    # Since e1071 svm predictions cannot return NA for missing data, we predict only for complete.cases and
     # no new factors.  Default to NA for other instances.
+    # droplevels after filtering for the cases that can be predicted.
     newdata[complete.cases(newdata) & !newdata$exclusions, "prediction"] <-
         predict(object$original, newdata = droplevels(newdata[complete.cases(newdata) & !newdata$exclusions, , drop = FALSE]))
     return(newdata$prediction)
@@ -158,19 +114,35 @@ predict.SupportVectorMachine <- function(object, newdata = NULL, na.action = na.
 #'
 #' Estimates probabilities of group membership for the entire sample passed into the original analysis (including missing and filtered values).
 #' @param x A \code{SupportVectorMachine} object.
-#' @importFrom stats na.pass
 #' @export
 Probabilities.SupportVectorMachine <- function(x)
 {
     if(x$numeric.outcome)
         stop("Probabilities are only applicable to models with categorical outcome variables.")
 
-    predict(x$original, decision.values = TRUE, probability = TRUE)
+    predictions <- predict(x$original, newdata = x$model, probability = TRUE)
+    prob.excluding.na <- attr(predictions, "probabilities")
+    # add NA probability for instances with missing prediction variables
+    all.probs <- data.frame(matrix(NA, ncol = ncol(prob.excluding.na), nrow = nrow(x$model)))
+    all.probs[row.names(prob.excluding.na), ] <- prob.excluding.na
+    colnames(all.probs) <- colnames(prob.excluding.na)
+    return(all.probs)
 }
 
 
-
-AlignPredictionVariables <- function(object, newdata = NULL)
+#' \code{AlignPredictionVariables}
+#'
+#' Verifies that newdata is consistent with data used to used to fit a model.  newdata must contain a
+#' superset of the variables used to fit the model or a stop error results. If a factor variable of
+#' newdata contains fewer levels than the factor used for fitting, the levels are expanded.  If a factor
+#' variable contains more levels than the factor used for fitting, a warning is given.  Returns newdata
+#' with potentially expanded factor levels and an 'exclusions' flag indicating which instances can
+#' be used for prediction.
+#' @param object A \code{SupportVectorMachine} object.
+#' @param newdata Optionally, a data frame including the variables used to fit the model.
+#' If omitted, the actual data used to fit the model is used (before any filtering).
+#' @export
+AlignPredictionVariables <- function(object, newdata)
 {
     if (is.null(newdata))
     {
@@ -184,13 +156,12 @@ AlignPredictionVariables <- function(object, newdata = NULL)
     # encodes factor variables according to the number of their levels and will fail if newdata contains
     # a different number of levels from training data (even if no instances have the unused levels).
     # Hence we filter out instances of newdata with new levels (predicting NA), and add back to newdata any
-    # levels not present but were in training data.  Thus droplevels(newdata) is aligned with training levels.
+    # levels not present but were in training data.  Thus droplevels(newdata) is aligned with fitted levels.
 
     training <- object$model[object$subset, names(object$model) != object$outcome.name, drop = FALSE]
     train.levels <- sapply(droplevels(training), levels)
 
     newdata <- newdata[, names(newdata) %in% names(training), drop = FALSE]
-    #newdata <- newdata[, names(newdata) != object$outcome.name, drop = FALSE]
     prediction.levels <- sapply(newdata, levels)
 
     if (!identical(names(training), names(newdata)))
