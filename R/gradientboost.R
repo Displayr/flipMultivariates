2e2d675a766dc97264e86aa15709c81a55e38565
jakehoare <jake_hoare@hotmail.com>
2017-05-09 11:53:17 +1000

    Initial implementation of GradientBoost

---
+++ R/gradientboost.R
@@ -0,0 +1,227 @@
+#' \code{GradientBoost}
+#'
+#' GradientBoost
+#' @param formula A formula of the form \code{groups ~ x1 + x2 + ...}
+#' That is, the response is the grouping factor and the right hand side
+#' specifies the (non-factor) discriminators, and any transformations, interactions,
+#' or other non-additive operators will be ignored.
+#' @param data A \code{\link{data.frame}} from which variables specified
+#' in formula are preferentially to be taken.
+#' @param subset An optional vector specifying a subset of observations to be
+#'   used in the fitting process, or, the name of a variable in \code{data}. It
+#'   may not be an expression.
+#' @param weights An optional vector of sampling weights, or the
+#'   name of a variable in \code{data}. It may not be an expression.
+#' @param output One of \code{"Accuracy"}, \code{"Importance"},
+#'   \code{"Prediction-Accuracy Table"} or \code{"Detail"}.
+#' @param missing How missing data is to be treated. Options:
+#'   \code{"Error if missing data"},
+#'   \code{"Exclude cases with missing data"}.
+#' @param booster Whether the underlying model is a tree or linear regression. Options:
+#'   \code{"gbtree"},
+#'   \code{"gblinear"}.
+#' @param seed The random number seed used in imputation.
+#' @param show.labels Shows the variable labels, as opposed to the labels, in the outputs, where a
+#' variables label is an attribute (e.g., attr(foo, "label")).
+#' @param ... Other arguments to be supplied to \code{\link{xgboost}}.
+#' @importFrom flipData GetData EstimationData DataFormula
+#' @importFrom flipFormat Labels
+#' @importFrom flipU OutcomeName
+#' @importFrom xgboost xgboost
+#' @importFrom flipTransformations AdjustDataToReflectWeights OneHot
+#' @export
+GradientBoost <- function(formula,
+                                 data = NULL,
+                                 subset = NULL,
+                                 weights = NULL,
+                                 output = "Accuracy",
+                                 missing  = "Exclude cases with missing data",
+                                 booster = "gbtree",
+                                 seed = 12321,
+                                 show.labels = FALSE,
+                                 ...)
+{
+    ####################################################################
+    ##### Reading in the data and doing some basic tidying        ######
+    ####################################################################
+    cl <- match.call()
+    input.formula <- formula # To work past scoping issues in car package: https://cran.r-project.org/web/packages/car/vignettes/embedding.pdf.
+    subset.description <- try(deparse(substitute(subset)), silent = TRUE) #We don't know whether subset is a variable in the environment or in data.
+    subset <- eval(substitute(subset), data, parent.frame())
+    if (!is.null(subset))
+    {
+        if (is.null(subset.description) | (class(subset.description) == "try-error") | !is.null(attr(subset, "name")))
+            subset.description <- Labels(subset)
+        if (is.null(attr(subset, "name")))
+            attr(subset, "name") <- subset.description
+    }
+    if(!is.null(weights))
+        if (is.null(attr(weights, "name")))
+            attr(weights, "name") <- deparse(substitute(weights))
+    weights <- eval(substitute(weights), data, parent.frame())
+    data <- GetData(input.formula, data, auxiliary.data = NULL)
+    row.names <- rownames(data)
+    outcome.name <- OutcomeName(input.formula)
+    outcome.i <- match(outcome.name, names(data))
+    outcome.variable <- data[, outcome.i]
+    numeric.outcome <- !is.factor(outcome.variable)
+    variable.labels <- Labels(data)
+    outcome.label <- variable.labels[outcome.i]
+    if (outcome.label == "data[, outcome.name]")
+        outcome.label <- outcome.name
+    if (!is.null(weights) & length(weights) != nrow(data))
+        stop("'weights' and 'data' are required to have the same number of observations. They do not.")
+    if (!is.null(subset) & length(subset) > 1 & length(subset) != nrow(data))
+        stop("'subset' and 'data' are required to have the same number of observations. They do not.")
+
+    # Treatment of missing values.
+    processed.data <- EstimationData(input.formula, data, subset, weights, missing, seed = seed)
+    unfiltered.weights <- processed.data$unfiltered.weights
+    .estimation.data <- processed.data$estimation.data
+    n.predictors <- ncol(.estimation.data)
+    n <- nrow(.estimation.data)
+    if (n < ncol(.estimation.data) + 1)
+        stop("The sample size is too small for it to be possible to conduct the analysis.")
+    .weights <- processed.data$weights
+    .formula <- DataFormula(input.formula)
+
+    # Resampling to generate a weighted sample, if necessary.
+    .estimation.data.1 <- if (is.null(weights))
+        .estimation.data
+    else
+        AdjustDataToReflectWeights(.estimation.data, .weights)
+
+    numeric.data <- OneHot(.estimation.data.1, outcome.name)
+
+    if (numeric.outcome)
+    {
+        objective <- "reg:linear"
+    }
+    else if (nlevels(.estimation.data[, outcome.name]) == 2)
+    {
+        objective <- "binary:logistic"
+    }
+    else
+    {
+        objective <- "multi:softmax"
+    }
+
+    ####################################################################
+    ##### Fitting the model. Ideally, this should be a call to     #####
+    ##### another function, with the output of that function       #####
+    ##### called 'original'.                                       #####
+    ####################################################################
+    set.seed(seed)
+    result <- list(original = xgboost(data = numeric.data$X, label = numeric.data$y,
+                                      params = list(booster = booster, objective = objective),
+                                      nrounds = 100, save_period = NULL, ...))
+    result$original$call <- cl
+
+    ####################################################################
+    ##### Saving results, parameters, and tidying up               #####
+    ####################################################################
+    # 1. Saving data.
+    result$subset <- subset <- row.names %in% rownames(.estimation.data)
+    result$weights <- unfiltered.weights
+    result$model <- data
+
+    # 2. Saving descriptive information.
+    class(result) <- "GradientBoost"
+    result$outcome.name <- outcome.name
+    result$sample.description <- processed.data$description
+    result$n.observations <- n
+    result$estimation.data <- .estimation.data
+    result$numeric.outcome <- numeric.outcome
+
+    # 3. Replacing names with labels
+    if (result$show.labels <- show.labels)
+    {
+        result$outcome.label <- outcome.label
+        result$variable.labels <- variable.labels <- variable.labels[-outcome.i]
+    }
+    else
+        result$outcome.label <- outcome.name
+
+    # 4. Saving parameters and confusion matrix
+    result$formula <- input.formula
+    result$output <- output
+    result$missing <- missing
+    result$confusion <- ConfusionMatrix(result, subset, unfiltered.weights)
+    result
+}
+
+#' @importFrom flipFormat DeepLearningTable FormatWithDecimals ExtractCommonPrefix
+#' @importFrom flipData GetTidyTwoDimensionalArray Observed
+#' @importFrom flipU IsCount
+#' @importFrom utils read.table
+#' @importFrom xgboost xgb.importance xgb.plot.importance
+#' @export
+print.GradientBoost <- function(x, ...)
+{
+    if (x$output == "Accuracy")
+    {
+        title <- paste0("Gradient Boost: ", x$outcome.label)
+        if (x$show.labels)
+        {
+            predictors <- x$variable.labels
+        }
+        else
+        {
+            predictors <- all.vars(x$formula[[3]])
+        }
+
+        extracted <- ExtractCommonPrefix(predictors)
+        if (!is.na(extracted$common.prefix))
+        {
+            predictors <- extracted$shortened.labels
+        }
+        predictors <- paste(predictors, collapse = ", ")
+
+        if (!x$numeric.outcome)
+        {
+            confM <- x$confusion
+            tot.cor <- sum(diag(confM))/sum(confM)
+            class.cor <- unlist(lapply(1:nrow(confM), function(i) {confM[i,i]/sum(confM[i,])}))
+            names(class.cor) <- colnames(confM)
+            subtitle <- sprintf("Overall Accuracy: %.2f%%", tot.cor*100)
+            tbl <- DeepLearningTable(class.cor*100,
+                                     column.labels = "Accuracy by class (%)",
+                                     order.values = FALSE,
+                                     title = title,
+                                     subtitle = paste(subtitle, " (Predictors: ", predictors, ")", sep = ""),
+                                     footer = x$sample.description)
+        }
+        else
+        {
+            obs <- Observed(x)[x$subset == TRUE]    # subset also accounts for NAs
+            pred <- predict(x)[x$subset == TRUE]
+            rmse <- sqrt(mean((pred - obs)^2))
+            rsq <- (cor(pred, obs))^2
+            subtitle <- "Measure of fit"
+            tbl <- DeepLearningTable(c("Root Mean Squared Error" = rmse, "R-squared" = rsq),
+                                     column.labels = " ",
+                                     order.values = FALSE,
+                                     title = title,
+                                     subtitle = paste(subtitle, " (Predictors: ", predictors, ")", sep = ""),
+                                     footer = x$sample.description)
+        }
+        print(tbl)
+
+    }
+    else if (x$output == "Prediction-Accuracy Table")
+    {
+        print(x$confusion)
+    }
+    else if (x$output == "Importance")
+    {
+        importance <- xgb.importance(feature_names = all.vars(x$formula[[3]]), model = x$original)
+        xgb.plot.importance(importance, rel_to_first = TRUE, xlab = "Relative importance")
+    }
+    else
+    {
+        print(x$original)
+        invisible(x)
+    }
+}
+
+
--- R/variables.R
+++ R/variables.R
@@ -92,19 +92,17 @@ randomForestExtractVariables <- function(object, type, newdata = object$model, n
 #' \code{predict.SupportVectorMachine}
 #'
 #' Predicts values for numeric outcomes and group membership for categories based on \code{newdata}
-#' and a fitted SupportVectorMachine \code{object}.  A value (which may be NA) is returned for every instance
-#' including those with missing data and for the fitted \code{data} before filtering in the case
-#' that \code{newdata} is not specified.  NA is returned for cases with unfitted factor levels.
+#' and a fitted \code{SupportVectorMachine} object.  A value (which may be NA) is returned for every instance
+#' including those with missing data. If \code{newdata} is not specified the fitted \code{data} before filtering
+#' are used.  NA is returned for cases with unfitted factor levels or any missing data.
 #' @param object A \code{SupportVectorMachine} object.
 #' @param newdata Optionally, a data frame including the variables used to fit the model.
 #' If omitted, the \code{data} supplied to \code{SupportVectorMachine()} is used before any filtering.
-#' @param na.action Function determining what should be done with missing values in \code{newdata}.
-#' The default is to predict \code{NA}.
 #' @param ... Additional arguments to pass to predict.svm.
-#' @importFrom stats na.omit complete.cases
+#' @importFrom stats complete.cases
 #' @importFrom flipData CheckPredictionVariables
 #' @export
-predict.SupportVectorMachine <- function(object, newdata = object$model, na.action = na.omit, ...)
+predict.SupportVectorMachine <- function(object, newdata = object$model, ...)
 {
     # CheckPredictionVariables is still required without newdata because predictions in object$fitted may be
     # a subset of object$model.
@@ -112,7 +110,7 @@ predict.SupportVectorMachine <- function(object, newdata = object$model, na.acti
     # Since e1071 svm predictions cannot return NA for missing data, we predict only for complete.cases
     # (without NA or new levels). Default to NA for other instances.
     newdata[complete.cases(newdata), "prediction"] <-
-        predict(object$original, newdata = newdata[complete.cases(newdata), , drop = FALSE])
+        predict(object$original, newdata = newdata[complete.cases(newdata), , drop = FALSE], ...)
     return(newdata$prediction)
 }
 
@@ -134,4 +132,48 @@ Probabilities.SupportVectorMachine <- function(x)
     colnames(all.probs) <- colnames(prob.excluding.na)
     return(all.probs)
 }
+
+#' \code{predict.GradientBoost}
+#'
+#' Predicts values for numeric outcomes and group membership for categories based on \code{newdata}
+#' and a fitted \code{GradientBoost} object.  A value (which may be NA) is returned for every instance
+#' including those with missing data. If \code{newdata} is not specified the fitted \code{data} before filtering
+#' are used.  NA is returned for cases with unfitted factor levels or any missing data.
+#' @param object A \code{GradientBoost} object.
+#' @param newdata Optionally, a data frame including the variables used to fit the model.
+#' If omitted, the \code{data} supplied to \code{GradientBoost()} is used before any filtering.
+#' @param ... Additional arguments to pass to predict.xgb.Booster.
+#' @importFrom stats complete.cases
+#' @importFrom flipData CheckPredictionVariables
+#' @export
+predict.GradientBoost <- function(object, newdata = object$model, ...)
+{
+    newdata <- CheckPredictionVariables(object, newdata)
+    newdata <- OneHot(newdata, object$outcome.name)$X
+    # Since xgboost predicts regardless of missing data, overwrite with NA if not complete.cases
+    prediction <- predict(object$original, newdata = newdata, ...)
+    prediction[!complete.cases(newdata)] <- NA
+    return(prediction)
+}
+
+#' \code{Probabilities.GradientBoost}
+#'
+#' Estimates probabilities of group membership for the entire sample passed into the original analysis (including
+#' missing and filtered values).
+#' @param x A \code{GradientBoost} object.
+#' @export
+Probabilities.GradientBoost <- function(x)
+{
+    if(x$numeric.outcome)
+        stop("Probabilities are only applicable to models with categorical outcome variables.")
+
+    predictions <- predict(x$original, newdata = x$model, probability = TRUE)
+    prob.excluding.na <- attr(predictions, "probabilities")
+    # add NA probability for instances with missing prediction variables
+    all.probs <- data.frame(matrix(NA, ncol = ncol(prob.excluding.na), nrow = nrow(x$model)))
+    all.probs[row.names(prob.excluding.na), ] <- prob.excluding.na
+    colnames(all.probs) <- colnames(prob.excluding.na)
+    return(all.probs)
+}
+
 
