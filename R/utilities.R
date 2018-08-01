899d58d490ed82d48085cf52e64812e1e9442804
jakehoare <jake_hoare@hotmail.com>
2018-08-01 14:44:50 +1000

    DS-1817 Stop LDA creating dummy variables with from missing levels.

--- R/utilities.R
+++ R/utilities.R
@@ -32,8 +32,15 @@ prepareMachineLearningData <- function(formula, data, subset, subset.description
     row.names <- rownames(data)
     outcome.name <- OutcomeName(input.formula, data)
     outcome.i <- match(outcome.name, names(data))
-    if (dummy)  # convert factors witn N levels to N-1 binary variables, used by LDA
+
+    if (dummy)
+    {
+        # convert factors witn N levels to N-1 binary variables, used by LDA
         data <- cbind(data[outcome.i], AsNumeric(data[, -outcome.i], binary = TRUE, remove.first = TRUE))
+        # remove constant variables caused by unpopulated levels
+        data[, -1] <- data[, -1][, lapply(data[, -1], var, na.rm = TRUE) != 0]
+    }
+
     if (setequal(data[!is.na(data[, outcome.i]), outcome.i], c(0, 1)))
         data[, outcome.i] <- as.factor(data[, outcome.i])
     outcome.variable <- data[, outcome.i]
