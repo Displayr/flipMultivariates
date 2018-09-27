e0f6bee19c260c6829a2d6318b50280abb6317d0
jakehoare <jake_hoare@hotmail.com>
2018-09-27 12:23:32 +1000

    Fix rebuilt formula with predictor names instead of dot to avoid problems with backticks.

--- R/utilities.R
+++ R/utilities.R
@@ -60,7 +60,9 @@ prepareMachineLearningData <- function(formula, data, subset, subset.description
 
     # Treatment of missing values.
     # rebuild formula because there are new variables for each level of factorial predictors
-    form <- as.formula(paste(outcome.name, "~ ."))
+    predictor.names <- colnames(data)[colnames(data) != outcome.name]
+    form <- as.formula(paste(outcome.name, "~",
+                             paste(predictor.names[predictor.names != outcome.name], collapse = "+")))
     processed.data <- EstimationData(form, data, subset, weights, missing, seed = seed)
     unweighted.training.data <- processed.data$estimation.data
     ErrorIfInfinity(unweighted.training.data)
