1838f100f097cf325439c003847504f98eabf2ab
Mathew W. McLean <mathew.w.mclean@gmail.com>
2023-05-31 14:33:26 +1000

    DS-4695: Add test files for parallelism

---
+++ tests/testthat_CCI1.R
@@ -0,0 +1,9 @@
+if (identical(Sys.getenv("CIRCLECI"), "true"))
+{
+    if (!dir.exists("reports"))
+        dir.create("reports")
+    out.file <- paste0("reports/test_results", Sys.getenv("CIRCLE_NODE_INDEX"), ".xml")
+    exit.code <- flipDevTools::RunTestsOnCircleCI(filter = "ensemble",
+                                                  load_package = "library", output_file = out.file)
+    q(status = exit.code, save = "no")
+}
