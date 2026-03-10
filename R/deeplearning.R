3b9989172db48c9c2bb66f48782f6353437f7ed7
jrwishart <justin.wishart@displayr.com>
2026-03-11 10:21:59 +1100

    Update keras implementation

--- R/deeplearning.R
+++ R/deeplearning.R
@@ -88,7 +88,7 @@ DeepLearning <- function(formula,
     X <- as.matrix(AsNumeric(unweighted.training.data[, -prepared.data$outcome.i]))
     if (normalize)
     {
-        means <- apply(X, 2, mean)
+        means <- colMeans(X)
         stdevs <- apply(X, 2, sd)
         X[, stdevs != 0] <- scale(X[, stdevs != 0], center = means[stdevs != 0], scale = stdevs[stdevs != 0])
     }
@@ -144,42 +144,35 @@ DeepLearning <- function(formula,
     result
 }
 
+#' @importFrom keras3 load_model
+#' @importFrom reticulate py_is_null_xptr
 prepareDLChartData <- function(x, ...)
 {
-    if (py_is_null_xptr(x$original))
-        x$original <- unserialize_model(x$original.serial)
-
-    if (x$output == "Accuracy")
-    {
-        return(calcAccuracy(x))
+    if (py_is_null_xptr(x[["original"]])) {
+        x$original <- load_model(x[["original.serial"]])
     }
-    else if (x$output == "Prediction-Accuracy Table")
-    {
-        return(ExtractChartData(x$confusion))
-    }
-    else if (x$output == "Cross Validation")
-    {
-        output.data <- data.frame(x$cross.validation$metrics)
-        colnames(output.data) <- c("Training accuracy", "Training loss", "Validation accuracy", "Validation loss")
-        return(output.data)
-    }
-    else
-    {
-        return(as.matrix(capture.output(print(x$original))))
+    output.type <- x[["output"]]
+    if (output.type == "Accuracy") {
+        calcAccuracy(x)
+    } else if (output.type == "Prediction-Accuracy Table") {
+        ExtractChartData(x[["confusion"]])
+    } else if (output.type == "Cross Validation") {
+        data.frame(x[["cross.validation"]][["metrics"]]) |>
+            setNames(c("Training accuracy", "Training loss", "Validation accuracy", "Validation loss"))
+    } else {
+        as.matrix(capture.output(print(x[["original"]])))
     }
 }
 
-#' @importFrom keras keras_model_sequential optimizer_rmsprop %>% layer_dense layer_dropout to_categorical
-#' @importFrom keras compile fit serialize_model callback_early_stopping shape
-#' @importFrom reticulate py_set_seed
-#' @importFrom tensorflow tf
+#' @importFrom keras3 keras_model_sequential optimizer_rmsprop layer_dense layer_dropout to_categorical
+#' @importFrom keras3 compile fit callback_early_stopping set_random_seed save_model
 neuralNetwork <- function(X,
                           Y,
                           hidden.nodes = c(256, 128),
                           max.epochs = 100,
-                          activation.functions = c('relu'),
+                          activation.functions = "relu",
                           dropout.rates = c(),
-                          optimizer = optimizer_rmsprop(),
+                          optimizer = function() optimizer_rmsprop(),
                           metrics = c(),
                           batch.size = 128,
                           numeric.outcome = TRUE,
@@ -187,75 +180,61 @@ neuralNetwork <- function(X,
                           rand.verbose = FALSE,
                           weights = NULL) {
 
-    # If using tensorflow>=2.12, use legacy optimizer
-    tf.above.2.11 <- package_version(tensorflow::tf$version$VERSION) >= package_version("2.12")
-    if (tf.above.2.11)
-        optimizer <- tensorflow::tf$optimizers$legacy$RMSprop()
-
-    # Note - below disables GPU computations and CPU parallelization by default, so slows performance
-    # This should be removed when we require more speed
-    # https://keras.rstudio.com/articles/faq.html#how-can-i-obtain-reproducible-results-using-keras-during-development
-    #use_session_with_seed(seed)
-
-    # This line is used instead of use_session_with_seed to avoid bug with tf v2.0.0
-    # https://github.com/rstudio/keras/issues/890
-    py_set_seed(seed)
-    tensorflow::tf$random$set_seed(seed)
-    rtmp <- tensorflow::tf$random$uniform(shape(2,2)) # to fix DS-2879
-    if (rand.verbose)
-    {
+    set_random_seed(seed)
+    if (rand.verbose) {
         cat("Seed:", seed, "\n")
-        cat("Some random numbers: ")
-        print(rtmp)
     }
 
+    if (length(activation.functions) < length(hidden.nodes)) {
+        last.activation.function <- tail(activation.functions, 1L)
+        activation.functions <- c(
+            activation.functions,
+            rep(last.activation.function, length(hidden.nodes) - length(activation.functions))
+        )
+    }
+
+    # defaults for numeric outcome
+    output.shape <- 1
+    output.activation <- NULL
+    cost.function <- "mse"
+    metrics <- "mae"
+    if (!numeric.outcome) {
+        metrics <- "accuracy"
+        if (NCOL(Y) > 1) {
+            output.shape <- NCOL(Y)
+            output.activation <- "softmax"
+            cost.function <- "categorical_crossentropy"
+        } else {
+            output.activation <- "sigmoid"
+            cost.function <- "binary_crossentropy"
+        }
+    }
     # create a function that builds the model one layer at a time.
     # called for cross-validation then again for the final model
     build.model <- function() {
         model <- keras_model_sequential()
-
         # hidden layers
-        for (i in 1:(length(hidden.nodes))) {
-            if (i == 1) {
-                # input layer
-                model %>% layer_dense(units = hidden.nodes[1], activation = activation.functions[1], input_shape = ncol(X))
-            } else {
-                activation = if (i <= length(activation.functions)) activation.functions[i] else activation.functions[length(activation.functions)]
-                model %>% layer_dense(units = hidden.nodes[i], activation = activation)
-            }
-
+        for (i in seq_along(hidden.nodes)) {
+            # input layer
+            model <- layer_dense(
+                model,
+                units = hidden.nodes[i],
+                activation = activation.functions[i],
+                input_shape = ncol(X)
+            )
             if (i <= length(dropout.rates)) {
-                model %>% layer_dropout(rate = dropout.rates[i]);
+                model <- layer_dropout(model, rate = dropout.rates[i])
             }
         }
-
-        # defaults for numeric outcome
-        output.shape <- 1
-        output.activation <- NULL
-        cost.function <- "mse"
-        metrics <- c("mae")
-
-        if (!numeric.outcome) {
-            metrics <- c("accuracy")
-            if (NCOL(Y) > 1) {
-                output.shape <- NCOL(Y)
-                output.activation <- "softmax"
-                cost.function <- "categorical_crossentropy"
-            } else {
-                output.activation <- "sigmoid"
-                cost.function <- "binary_crossentropy"
-            }
-        }
-
         # output layer
-        model %>% layer_dense(units = output.shape, activation = output.activation)
+        model <- layer_dense(model, output.shape, activation = output.activation)
 
-        model %>% compile(
+        model |> compile(
             loss = cost.function,
-            optimizer = optimizer,
+            optimizer = optimizer(),
             metrics = metrics
         )
-        return(model)
+        model
     }
 
     # randomly shuffle the data
@@ -273,20 +252,23 @@ neuralNetwork <- function(X,
         callbacks = c(callback_early_stopping(patience = 3)),
         verbose = 0
     )
-    fit.args[[if (tf.above.2.11) "weighted_metrics" else "sample_weights"]] <- weights
+    if (!is.null(weights)) {
+        fit.args[["sample_weight"]] <- weights
+    }
     history <- do.call(fit, fit.args)
-    history$metrics <- history$metrics[sort(names(history$metrics))]    # fix unstable ordering
-    history$params <- history$params[sort(names(history$params))]
+    history[["metrics"]] <- history[["metrics"]][sort(names(history[["metrics"]]))]    # fix unstable ordering
+    history[["params"]] <- history[["params"]][sort(names(history[["params"]]))]
 
-    if ((optimal.epochs <- length(history$metrics$val_loss)) == max.epochs)
+    if ((optimal.epochs <- length(history[["metrics"]][["val_loss"]])) == max.epochs)
         warning("Cross validation loss is still decreasing after maximum number of epochs.",
                 " Model may not have converged, consider increasing the maximum number of epochs.")
     else
-        optimal.epochs <- optimal.epochs - 3
+        optimal.epochs <- max(1, optimal.epochs - 3)
 
     # retrain on all data for optimal number of epochs
     model <- build.model()
-    model %>% fit(
+    fit(
+        model,
         X,
         Y,
         epochs = optimal.epochs,
@@ -294,9 +276,11 @@ neuralNetwork <- function(X,
         verbose = 0
     )
 
-    return(list(original = model,
-                original.serial = serialize_model(model),
-                cross.validation = padMetrics(history)))
+    list(
+        original = model,
+        original.serial = save_model(model, filepath = NULL),
+        cross.validation = padMetrics(history)
+    )
 }
 
 # This function fixes up a bug in keras::plot.keras_training_history
@@ -320,25 +304,25 @@ padMetrics <- function(cv)
 #' @importFrom flipData Observed
 #' @importFrom flipU IsCount
 #' @importFrom utils read.table
-#' @importFrom keras unserialize_model
+#' @importFrom keras3 load_model
 #' @importFrom reticulate py_is_null_xptr
 #' @importFrom graphics plot
 #' @export
 #' @method print DeepLearning
 print.DeepLearning <- function(x, ...)
 {
-    if (py_is_null_xptr(x$original))
-        x$original <- unserialize_model(x$original.serial)
+    if (py_is_null_xptr(x[["original"]])) {
+        x[["original"]] <- unserialize_model(x[["original.serial"]])
+    }
 
-    if (x$output == "Accuracy")
+    if (x[["output"]] == "Accuracy") {
         print(formatAccuracy(x, "Deep Learning"))
-    else if (x$output == "Prediction-Accuracy Table")
-        print(x$confusion)
-    else if (x$output == "Cross Validation")
-        print(plot(x$cross.validation))
-    else
-    {
-        print(x$original)
+    } else if (x[["output"]] == "Prediction-Accuracy Table") {
+        print(x[["confusion"]])
+    } else if (x[["output"]] == "Cross Validation") {
+        print(plot(x[["cross.validation"]]))
+    } else {
+        print(x[["original"]])
         invisible(x)
     }
 }
@@ -355,42 +339,47 @@ print.DeepLearning <- function(x, ...)
 #' If omitted, the \code{data} supplied to \code{DeepLearning()} is used before any filtering.
 #' @param ... Additional arguments to pass to predict.DeepLearning.
 #' @importFrom flipData CheckPredictionVariables ValidateNewData
-#' @importFrom keras k_argmax unserialize_model
+#' @importFrom keras3 op_argmax load_model
 #' @importFrom reticulate py_is_null_xptr
 #' @export
 predict.DeepLearning <- function(object, newdata = NULL, ...)
 {
-    if (py_is_null_xptr(object$original))
-        object$original <- unserialize_model(object$original.serial)
+    if (py_is_null_xptr(object[["original"]])) {
+        object[["original"]] <- load_model(object[["original.serial"]])
+    }
 
     newdata <- ValidateNewData(object, newdata)
 
     X <- as.matrix(AsNumeric(newdata))
-    constants <- object$training.stdevs == 0
-    if (object$normalize)
-        X[, !constants] <- scale(X[, !constants, drop = FALSE],
-                                 center = object$training.means[!constants],
-                                 scale = object$training.stdevs[!constants])
-
-    if (!object$numeric.outcome)
-    {
-        if (length(object$outcome.levels) > 2L)
-            predictions <- as.numeric(k_argmax(predict(object$original, X)))
-        else
-            predictions <- predict(object$original, X) > 0.5
-
-        predictions <- factor(predictions + 1L,
-                              levels = seq(length(object$outcome.levels)),
-                              labels = object$outcome.levels)
+    constants <- object[["training.stdevs"]] == 0
+    if (object[["normalize"]]) {
+        X[, !constants] <- scale(
+            X[, !constants, drop = FALSE],
+            center = object[["training.means"]][!constants],
+            scale = object[["training.stdevs"]][!constants]
+        )
     }
-    else
-        predictions <- predict(object$original, X)
+    if (!object[["numeric.outcome"]])
+    {
+        predictions <- if (length(object[["outcome.levels"]]) > 2L) {
+            as.numeric(op_argmax(predict(object[["original"]], X), axis = 1L))
+        } else {
+            predict(object[["original"]], X) > 0.5
+        }
 
-    return(predictions)
+        predictions <- factor(
+            predictions + 1L,
+            levels = seq_along(object[["outcome.levels"]]),
+            labels = object[["outcome.levels"]]
+        )
+    } else {
+        predictions <- predict(object[["original"]], X)
+    }
+    predictions
 }
 
 #' @importFrom flipData ValidateNewData
-#' @importFrom keras unserialize_model
+#' @importFrom keras3 load_model
 #' @importFrom reticulate py_is_null_xptr
 #' @export
 Probabilities.DeepLearning <- function(object, newdata = NULL, ...)
@@ -398,19 +387,24 @@ Probabilities.DeepLearning <- function(object, newdata = NULL, ...)
     requireCategoricalOutcome(object)
     newdata <- ValidateNewData(object, newdata)
 
-    if (py_is_null_xptr(object$original))
-        object$original <- unserialize_model(object$original.serial)
+    if (py_is_null_xptr(object[["original"]])) {
+        object[["original"]] <- unserialize_model(object[["original.serial"]])
+    }
 
     X <- as.matrix(AsNumeric(newdata))
-    constants <- object$training.stdevs == 0
-    if (object$normalize)
-        X[, !constants] <- scale(X[, !constants],
-                                 center = object$training.means[!constants],
-                                 scale = object$training.stdevs[!constants])
+    constants <- object[["training.stdevs"]] == 0
+    if (object[["normalize"]]) {
+        X[, !constants] <- scale(
+            X[, !constants],
+            center = object[["training.means"]][!constants],
+            scale = object[["training.stdevs"]][!constants]
+        )
+    }
 
-    probabilities <- predict(object$original, X)
-    if (length(object$outcome.levels) == 2)
-        probabilities <- cbind(1 - probabilities,probabilities)
-    colnames(probabilities) <- object$outcome.levels
+    probabilities <- predict(object[["original"]], X)
+    if (length(object[["outcome.levels"]]) == 2) {
+        probabilities <- cbind(1 - probabilities ,probabilities)
+    }
+    colnames(probabilities) <- object[["outcome.levels"]]
     probabilities
 }
