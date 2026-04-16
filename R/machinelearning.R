53cdc1bd1aff9a7348d2461c55e4c052cf3f509a
Cadigal-displayr <cadigal.havens@displayr.com>
2026-04-16 13:04:37 +1000

    Add simulator functions

--- R/machinelearning.R
+++ R/machinelearning.R
@@ -153,3 +153,170 @@ parametersEqual <- function(recipient, donor) {
     return(FALSE)
 }
 
+#' organiseCategoricalPredictors
+#'
+#' Organize information about categorical predictors in the model
+#' @param input.model The machine learning model.
+#' @param all.combo.boxes The selection of predictor values.
+#' @noRd
+organiseCategoricalPredictors <- function(input.model, all.combo.boxes) {
+    estimation.data.template <- input.model$estimation.data.template
+    all.variable.names <- names(estimation.data.template)
+    outcome.name <- attr(estimation.data.template, "outcome.name")
+    all.predictor.names <- setdiff(all.variable.names, outcome.name)
+    predictor.templates <- estimation.data.template[all.predictor.names]
+    all.predictor.labels <- vapply(predictor.templates, "[[", character(1L), "label")
+    xlevels <- lapply(predictor.templates, FUN = function(this.var) {
+        if (!is.null(this.var[["observed.short.levels"]])) {
+            return(this.var[["observed.short.levels"]])
+        }
+        if (inherits(input.model, "LDA")) {
+            return(this.var[["levels"]])
+        }
+        this.var[["observed.levels"]]
+    })
+    names(xlevels) <- all.predictor.names
+    xlevels <- Filter(length, xlevels)
+    all.predictor.names <- names(xlevels)
+    levels.not.in.data <- mapply(
+        function(var.name, combo.name) !combo.name %in% xlevels[[var.name]],
+        all.predictor.names, all.combo.boxes
+    )
+    if (any(levels.not.in.data)) {
+        relevant.combos <- all.combo.boxes[levels.not.in.data]
+        relevant.vars <- all.predictor.names[levels.not.in.data]
+        matches <- mapply(function(var.name, combo.name) vapply(xlevels[[var.name]], function(x) sub(combo.name, "", x), character(1L)),
+            relevant.vars, relevant.combos,
+            SIMPLIFY = FALSE
+        )
+        white.space.diffs <- lapply(matches, function(x) grepl("^\\s+$", x))
+        if (any(unlist(white.space.diffs))) {
+            var.to.use <- which.max(vapply(white.space.diffs, any, logical(1L)))
+            StopForUserError(
+                "This feature is not compatible with category labels that have surrounding white space. ",
+                "To use this feature the category ", dQuote(xlevels[[names(var.to.use)]][white.space.diffs[[var.to.use]]]),
+                " needs to be renamed to ", dQuote(relevant.combos[var.to.use]), " in the predictor variable ",
+                sQuote(all.predictor.labels[levels.not.in.data][var.to.use])
+            )
+        }
+        first.bad.level <- which.max(levels.not.in.data)
+        StopForUserError(
+            "The selected category ", dQuote(all.combo.boxes[first.bad.level]), " in the predictor variable ",
+            sQuote(all.predictor.labels[first.bad.level]), " is not observed in the dataset used to construct the model ",
+            "and consequently the model cannot predict the outcome. Please select a different category for this ",
+            "predictor variable."
+        )
+    }
+    return(xlevels)
+}
+
+#' predictOutcome
+#'
+#' Predict the outcome for a simulator for a machine learning model.
+#' @param input.model The machine learning model.
+#' @param DF The data frame containing the predictor values.
+#' @param is.numeric A logical value indicating if the outcome is numeric.
+#' @noRd
+predictOutcome <- function(input.model, DF, is.numeric) {
+    vector.or.class <- if (is.numeric) "vector" else "class"
+    is.cart <- inherits(input.model, "CART")
+    is.lda <- inherits(input.model, "LDA")
+    prediction.function <- predict
+    if (is.lda) {
+        DF <- flipTransformations::AsDataFrame(DF,
+            use.names = TRUE,
+            ignore.columns = "",
+            categorical.as.binary = TRUE,
+            remove.first = TRUE
+        )
+    }
+    arguments <- list(input.model, newdata = DF)
+    if (is.cart) {
+        prediction.function <- rpart:::predict.rpart
+        type <- vector.or.class
+        arguments <- c(arguments, type = type)
+    }
+    if (is.lda) {
+        prediction.function <- ldaExtractVariables
+        arguments$type <- "class"
+        arguments$prior <- input.model$prior
+        arguments$na.action <- na.pass
+    }
+    as.vector(do.call(prediction.function, arguments))
+}
+
+#' predictProbabilities
+#'
+#' Predict the probabilities for a simulator for a machine learning model.
+#' @param input.model The machine learning model.
+#' @param DF The data frame containing the predictor values.
+#' @noRd
+predictProbabilities <- function(input.model, DF) {
+    model.classes <- class(input.model)
+    if ("SupportVectorMachine" %in% model.classes) {
+        svm.probs <- e1071:::predict.svm(input.model$original, newdata = DF, probability = TRUE)
+        new.probs <- attr(svm.probs, "probabilities")
+    } else if ("RandomForest" %in% model.classes) {
+        new.probs <- randomForestExtractVariables(input.model, "prob", newdata = DF)
+    } else if ("DeepLearning" %in% model.classes) {
+        if (reticulate::py_is_null_xptr(input.model$original)) {
+            input.model$original <- keras::unserialize_model(input.model$original.serial)
+        }
+        X <- as.matrix(flipTransformations::AsNumeric(DF))
+        constants <- input.model$training.stdevs == 0
+        if (input.model$normalize) {
+            X[, !constants] <- scale(X[, !constants, drop = FALSE],
+                center = input.model$training.means[!constants],
+                scale = input.model$training.stdevs[!constants]
+            )
+        }
+        new.probs <- predict(input.model$original, X)
+        if (length(input.model$outcome.levels) == 2) {
+            new.probs <- cbind(1 - new.probs, new.probs)
+        }
+        colnames(new.probs) <- input.model$outcome.levels
+    } else if ("CART" %in% model.classes) {
+        new.probs <- tryCatch(rpart:::predict.rpart(input.model, newdata = DF, type = "prob"),
+            error = function(e) {
+                if (grepl("new level", e$message)) {
+                    StopForUserError("Cannot match categories. Please set Inputs > Predictor category labels to 'Full labels' in the CART model.")
+                } else {
+                    e
+                }
+            }
+        )
+    } else if ("GradientBoost" %in% model.classes) {
+        new.probs <- predict.GradientBoost(input.model, newdata = DF, keep.soft.probs = TRUE)
+        if (length(new.probs) == 1L) {
+            new.probs <- cbind(1 - new.probs, new.probs)
+        }
+        colnames(new.probs) <- input.model$outcome.levels
+    } else if ("LDA" %in% model.classes) {
+        DF <- flipTransformations::AsDataFrame(DF,
+            use.names = TRUE,
+            ignore.columns = "",
+            categorical.as.binary = TRUE,
+            remove.first = TRUE
+        )
+        new.probs <- ldaExtractVariables(input.model, "posterior", input.model$prior, newdata = DF, na.action = na.pass)
+    } else if ("BinaryLogitRegression" %in% model.classes) {
+        new.probs <- Probabilities(input.model, newdata = DF)
+    } else if ("MultinomialLogitRegression" %in% model.classes) {
+        new.probs <- flipRegression:::Probabilities.Regression(input.model, newdata = DF)
+        if (nrow(new.probs) == 1L) {
+            colnames(new.probs) <- input.model$original$lev
+        } else {
+            new.probs <- new.probs[, 2, drop = FALSE]
+        }
+    } else if ("OrderedLogitRegression" %in% model.classes) {
+        new.probs <- flipRegression:::Probabilities.Regression(input.model, newdata = rbind(DF, DF)) # Doesn't like new data with a single row
+        new.probs <- as.matrix(new.probs[1, , drop = FALSE])
+    }
+
+    if (ncol(new.probs) > 1) {
+        new.probs <- t(new.probs)
+    }
+    colnames(new.probs) <- "Probability (%)"
+    new.probs <- new.probs * 100
+    return(new.probs)
+}
