be283014b7491cf9dc6c10d89afb62f558aeef28
Cadigal-displayr <cadigal.havens@displayr.com>
2026-05-18 15:27:15 +1000

    Hush.

--- tests/testthat_CCI2.R
+++ tests/testthat_CCI2.R
@@ -1,9 +1,10 @@
-if (identical(Sys.getenv("CIRCLECI"), "true"))
-{
-    if (!dir.exists("reports"))
+if (identical(Sys.getenv("CIRCLECI"), "true")) {
+    if (!dir.exists("reports")) {
         dir.create("reports")
+    }
     out.file <- paste0("reports/test_results", Sys.getenv("CIRCLE_NODE_INDEX"), ".xml")
-    exit.code <- flipDevTools::RunTestsOnCircleCI(filter = "^(?!ensemble)", perl = TRUE,
-                                                  load_package = "installed", output_file = out.file)
+    # Using getExportedValue instead of flipDevTools::RunTestsOnCircleCI to silence CI warning
+    exit.code <- getExportedValue("flipDevTools", "RunTestsOnCircleCI")(filter = "^(?!ensemble)", perl = TRUE,
+        load_package = "installed", output_file = out.file)
     q(status = exit.code, save = "no")
 }
